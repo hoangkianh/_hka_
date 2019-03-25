111cd8dd84250707a75a19f7975681934dfef1e2
hka <hoangkianh31592@gmail.com>
2019-03-25 20:42:06 +0700

    remove skip-link-focus-fix

--- src/_hka_/functions.php
+++ src/_hka_/functions.php
@@ -150,8 +150,6 @@ function _hka__scripts() {
 
 	wp_enqueue_script( '_hka_-script', get_template_directory_uri() . '/assets/js/main.js', array(), _HKA__THEME_VERSION, true );
 
-	wp_enqueue_script( '_hka_-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _HKA__THEME_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
